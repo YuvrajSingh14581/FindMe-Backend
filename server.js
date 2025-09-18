07e2cfb2d097828ea2440cf0c63154d791a6d8d0
YUVRAJ SINGH <165543892+YuvrajSingh14581@users.noreply.github.com>
2025-09-18 23:07:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-frontend-domain.com'], // Add your frontend URLs
+  origin: ['http://localhost:5173', 'https://ffindme.netlify.app'], // Add your frontend URLs
   credentials: true
 }));
 app.use(express.json());
